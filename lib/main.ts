741de49d67faa5bd7a7af7ba8d65ec6f12627fde
qqyzk <biweichen@pku.edu.cn>
2024-01-10 14:23:48 +0800

    add scene1 recover

--- lib/main.ts
+++ lib/main.ts
@@ -41,6 +41,14 @@ function scene1(){
         }
     }
 
+    function recover(){
+        for(let i=0;i<sphereNum;i++){
+            spheres[i].sphere.scaling.x=scaling[i];
+            spheres[i].sphere.scaling.y=scaling[i];
+            spheres[i].sphere.scaling.z=scaling[i];
+        }
+    }
+
     function swing() {
         if(swingFirst){
             duck.rotation.x-=0.02*(swingNum/4);
@@ -76,9 +84,6 @@ function scene1(){
         sphere.speed=Math.random()*0.01+0.002;
     }
     
-   
-   
-   
     function fly(){
         timer++;
         if(timer==120){
@@ -135,16 +140,14 @@ function scene1(){
         scene.unregisterAfterRender(animate);
         duck.position=originalPosition;
         duck.rotation=originalRotation;
-
+        swingFirst=true;
     }
     return {
         init:init,
+        recover:recover,
         beginAnimation:beginAnimation,
         dispose:dispose
     };
-    // init();
-    // beginAnimation();
-
 }
 
 
@@ -156,6 +159,12 @@ function scene2(){
     let rotatep = 0;
     let rotateFirst=true;
     let rotateNum=200;
+    let pbrs=[];
+    let capsules=[];
+    let timer=0;
+    let blinkp = 0;
+    const originalPosition = duck.position.clone();
+    const originalRotation = duck.rotation.clone();
     function rotate() {
         if(rotateFirst){
             duck.position.y-=0.01*rotateNum/4
@@ -171,108 +180,106 @@ function scene2(){
         rotatep%=rotateNum;
     }
     
-    let pbrs=[];
-    let capsules=[];
-    const capsule1 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
-    capsule1.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
-    capsule1.position=new BABYLON.Vector3(0,2,-1);
-    var pbr1 = new BABYLON.PBRMaterial("pbr", scene);
-    capsule1.material = pbr1;
-    pbr1.metallic = Math.random();
-    pbr1.roughness = Math.random();   
-    pbr1.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbr1.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbrs.push(pbr1);
-    capsules.push(capsule1);
-    const capsule6 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
-    capsule6.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
-    capsule6.position=new BABYLON.Vector3(-0.7,1.7,-1);
-    var pbr6 = new BABYLON.PBRMaterial("pbr", scene);
-    capsule6.material = pbr6;
-    pbr6.metallic = Math.random();
-    pbr6.roughness = Math.random();   
-    pbr6.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbr6.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbrs.push(pbr6);
-    capsule6.rotation.z+=Math.PI/4;
-    capsules.push(capsule6);
-    const capsule3 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
-    capsule3.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
-    capsule3.position=new BABYLON.Vector3(-1,1,-1);
-    var pbr3 = new BABYLON.PBRMaterial("pbr", scene);
-    capsule3.material = pbr3;
-    pbr3.metallic = Math.random();
-    pbr3.roughness = Math.random();   
-    pbr3.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbr3.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbrs.push(pbr3);
-    capsule3.rotation.z+=Math.PI/2;
-    capsules.push(capsule3);
-    const capsule7 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
-    capsule7.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
-    capsule7.position=new BABYLON.Vector3(-0.7,0.3,-1);
-    var pbr7 = new BABYLON.PBRMaterial("pbr", scene);
-    capsule7.material = pbr7;
-    pbr7.metallic = Math.random();
-    pbr7.roughness = Math.random();   
-    pbr7.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbr7.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbrs.push(pbr7);
-    capsule7.rotation.z+=Math.PI*3/4;
-    capsules.push(capsule7);
-    const capsule2 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
-    capsule2.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
-    capsule2.position=new BABYLON.Vector3(0,0.15,-1);
-    var pbr2 = new BABYLON.PBRMaterial("pbr", scene);
-    capsule2.material = pbr2;
-    pbr2.metallic = Math.random();
-    pbr2.roughness = Math.random();   
-    pbr2.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbr2.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbrs.push(pbr2);
-    capsule2.rotation.x+=Math.PI;
-    capsules.push(capsule2);
-    const capsule8 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
-    capsule8.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
-    capsule8.position=new BABYLON.Vector3(0.7,0.3,-1);
-    var pbr8 = new BABYLON.PBRMaterial("pbr", scene);
-    capsule8.material = pbr8;
-    pbr8.metallic = Math.random();
-    pbr8.roughness = Math.random();   
-    pbr8.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbr8.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbrs.push(pbr8);
-    capsule8.rotation.z-=Math.PI*3/4;
-    capsules.push(capsule8);
-    const capsule4 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
-    capsule4.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
-    capsule4.position=new BABYLON.Vector3(1,1,-1);
-    var pbr4 = new BABYLON.PBRMaterial("pbr", scene);
-    capsule4.material = pbr4;
-    pbr4.metallic = Math.random();
-    pbr4.roughness = Math.random();   
-    pbr4.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbr4.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbrs.push(pbr4);
-    capsules.push(capsule4);
-    capsule4.rotation.z-=Math.PI/2;
-    const capsule5 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
-    capsule5.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
-    capsule5.position=new BABYLON.Vector3(0.7,1.7,-1);
-    var pbr5 = new BABYLON.PBRMaterial("pbr", scene);
-    capsule5.material = pbr5;
-    pbr5.metallic = Math.random();
-    pbr5.roughness = Math.random();   
-    pbr5.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbr5.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-    pbrs.push(pbr5);
-    capsule5.rotation.z-=Math.PI/4;
-    capsules.push(capsule5);
+    function init(){
+        const capsule1 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
+        capsule1.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
+        capsule1.position=new BABYLON.Vector3(0,2,-1);
+        var pbr1 = new BABYLON.PBRMaterial("pbr", scene);
+        capsule1.material = pbr1;
+        pbr1.metallic = Math.random();
+        pbr1.roughness = Math.random();   
+        pbr1.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbr1.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbrs.push(pbr1);
+        capsules.push(capsule1);
+        const capsule6 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
+        capsule6.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
+        capsule6.position=new BABYLON.Vector3(-0.7,1.7,-1);
+        var pbr6 = new BABYLON.PBRMaterial("pbr", scene);
+        capsule6.material = pbr6;
+        pbr6.metallic = Math.random();
+        pbr6.roughness = Math.random();   
+        pbr6.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbr6.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbrs.push(pbr6);
+        capsule6.rotation.z+=Math.PI/4;
+        capsules.push(capsule6);
+        const capsule3 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
+        capsule3.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
+        capsule3.position=new BABYLON.Vector3(-1,1,-1);
+        var pbr3 = new BABYLON.PBRMaterial("pbr", scene);
+        capsule3.material = pbr3;
+        pbr3.metallic = Math.random();
+        pbr3.roughness = Math.random();   
+        pbr3.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbr3.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbrs.push(pbr3);
+        capsule3.rotation.z+=Math.PI/2;
+        capsules.push(capsule3);
+        const capsule7 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
+        capsule7.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
+        capsule7.position=new BABYLON.Vector3(-0.7,0.3,-1);
+        var pbr7 = new BABYLON.PBRMaterial("pbr", scene);
+        capsule7.material = pbr7;
+        pbr7.metallic = Math.random();
+        pbr7.roughness = Math.random();   
+        pbr7.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbr7.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbrs.push(pbr7);
+        capsule7.rotation.z+=Math.PI*3/4;
+        capsules.push(capsule7);
+        const capsule2 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
+        capsule2.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
+        capsule2.position=new BABYLON.Vector3(0,0.15,-1);
+        var pbr2 = new BABYLON.PBRMaterial("pbr", scene);
+        capsule2.material = pbr2;
+        pbr2.metallic = Math.random();
+        pbr2.roughness = Math.random();   
+        pbr2.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbr2.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbrs.push(pbr2);
+        capsule2.rotation.x+=Math.PI;
+        capsules.push(capsule2);
+        const capsule8 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
+        capsule8.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
+        capsule8.position=new BABYLON.Vector3(0.7,0.3,-1);
+        var pbr8 = new BABYLON.PBRMaterial("pbr", scene);
+        capsule8.material = pbr8;
+        pbr8.metallic = Math.random();
+        pbr8.roughness = Math.random();   
+        pbr8.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbr8.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbrs.push(pbr8);
+        capsule8.rotation.z-=Math.PI*3/4;
+        capsules.push(capsule8);
+        const capsule4 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
+        capsule4.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
+        capsule4.position=new BABYLON.Vector3(1,1,-1);
+        var pbr4 = new BABYLON.PBRMaterial("pbr", scene);
+        capsule4.material = pbr4;
+        pbr4.metallic = Math.random();
+        pbr4.roughness = Math.random();   
+        pbr4.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbr4.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbrs.push(pbr4);
+        capsules.push(capsule4);
+        capsule4.rotation.z-=Math.PI/2;
+        const capsule5 = new BABYLON.Mesh.CreateCapsule("capsule", {radius:0.5, height:12, radiusTop:3});
+        capsule5.scaling=new BABYLON.Vector3(0.15,0.15,0.15);
+        capsule5.position=new BABYLON.Vector3(0.7,1.7,-1);
+        var pbr5 = new BABYLON.PBRMaterial("pbr", scene);
+        capsule5.material = pbr5;
+        pbr5.metallic = Math.random();
+        pbr5.roughness = Math.random();   
+        pbr5.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbr5.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        pbrs.push(pbr5);
+        capsule5.rotation.z-=Math.PI/4;
+        capsules.push(capsule5);
+    }
 
 
-
-    let timer=0;
-    let blinkp = 0;
+    
     function blink(){
         timer++;
         if(timer==120){
@@ -305,19 +312,39 @@ function scene2(){
         blink();
         sceneTime++;
     }
-    scene.registerAfterRender(animate);
-  
+
+    function beginAnimation(){
+        scene.registerAfterRender(animate);
+    }
+    function dispose(){
+       
+        for(let i=0;i<capsules.length;i++){
+            capsules[i].scaling.x=0;
+            capsules[i].scaling.y=0;
+            capsules[i].scaling.z=0;
+        }
+        scene.unregisterAfterRender(animate);
+        duck.position=originalPosition;
+        duck.rotation=originalRotation;
+    }
+    return {
+        init:init,
+        beginAnimation:beginAnimation,
+        dispose:dispose
+    };
 }
 
 
 let sceneType = Math.floor(Math.random()*2)+1;
-sceneType = 1;
+sceneType = 2;
 let scenes=[];
 scenes.push(scene1);
 scenes.push(scene2);
-let res=scene1();
-res.init();
-res.beginAnimation();
+// scene2();
+let res1=scene1();
+let res2=scene2();
+res1.init();
+res1.beginAnimation();
 // if(sceneType==1){
 //     let dispose=scene1();
 //     // dispose();
@@ -326,7 +353,14 @@ res.beginAnimation();
 // }
 // let dispose=scenes[sceneType-1]();
 setTimeout(() => {
-   res.dispose();
+   res1.dispose();
+   setTimeout(() => {
+        res1.recover();
+        res1.beginAnimation();
+    }, 10000);
+//    res.beginAnimation();
+    // res1.init();
+    // res1.beginAnimation();
 }, 10000);
 // scene=scene1();
 // scene.init();
